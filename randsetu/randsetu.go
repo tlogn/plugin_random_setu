a8dd831c9db4a3df063d2fc422cc1354823062f7
ylx <ylx@bytedance.com>
2022-04-30 15:14:27 +0800

    update

--- randsetu/randsetu.go
+++ randsetu/randsetu.go
@@ -82,7 +82,7 @@ func randSetuApi() (r resultjson, err error) {
 func downloadImageFromID(id int64) (string, error) {
 	illust, err := pixiv.Works(id)
 	if err != nil || len(illust.ImageUrls) == 0 || illust.ImageUrls[0] == "" {
-		return "", err
+		return "", errors.New("downloadImageFromId error")
 	}
 	/*
 		for err != nil || illust.ImageUrls[0] == "" {
